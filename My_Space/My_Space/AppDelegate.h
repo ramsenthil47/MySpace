67bffdb309048a8e60c5bc7ae907ae5caa27e103
Senthilram Appadurai <ramsenthil47@gmail.com>
2015-07-24 18:27:54 -0500

    Created the Project MySpace.

---
+++ My_Space/My_Space/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  My_Space
+//
+//  Created by Senthilram Appadurai on 7/24/15.
+//  Copyright (c) 2015 Senthilram Appadurai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
